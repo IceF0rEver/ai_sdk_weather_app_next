6a53981bc0b1337fe8977b17e04391151b3d117f
IceF0rEver <arnaudorban.pro@gmail.com>
2025-10-31 16:19:43 +0100

    fix: mist models

--- ai_sdk_weather_app/src/app/[locale]/dashboard/page.tsx
+++ ai_sdk_weather_app/src/app/[locale]/dashboard/page.tsx
@@ -3,12 +3,16 @@ import AiChat from "@/components/utils/ai/ai-chat";
 export default async function Page() {
 	const models = [
 		{
-			name: "Pixtral",
-			value: "pixtral-large-latest",
+			name: "Mistral small",
+			value: "mistral-small-latest",
 		},
 		{
 			name: "Mistral 3b",
 			value: "ministral-3b-latest",
+		},
+		{
+			name: "Pixtral",
+			value: "pixtral-large-latest",
 		},
 		{
 			name: "Magistral",
