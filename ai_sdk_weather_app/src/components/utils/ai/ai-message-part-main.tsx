420c957d1e52a3adac91ddca10c7cd4ce278a7df
IceF0rEver <arnaudorban.pro@gmail.com>
2025-11-07 18:53:36 +0100

    feat: weather

--- ai_sdk_weather_app/src/components/utils/ai/ai-message-part-main.tsx
+++ ai_sdk_weather_app/src/components/utils/ai/ai-message-part-main.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Fragment } from "react";
+import { AiToolWeather } from "@/app/[locale]/dashboard/_components/_ai/_tools/ai-tool-weather";
+import { AiToolWeathersList } from "@/app/[locale]/dashboard/_components/_ai/_tools/ai-tool-weathers-list";
 import { Response } from "@/components/ai-elements/response";
 import type { MyUIMessage } from "./_types/types";
 
@@ -20,6 +22,50 @@ export function AiMessageMainPart({ ...props }: AiMessageMainPartProps) {
 								<Response>{part.text}</Response>
 							</Fragment>
 						);
+					case "tool-getCurrentWeatherByLocation":
+						switch (part.state) {
+							case "output-available":
+								return (
+									<Fragment key={`${props.message.id}-${i}`}>
+										<AiToolWeather part={part.output} />
+									</Fragment>
+								);
+							default:
+								return null;
+						}
+					case "tool-getCurrentWeatherByName":
+						switch (part.state) {
+							case "output-available":
+								return (
+									<Fragment key={`${props.message.id}-${i}`}>
+										<AiToolWeather part={part.output} />
+									</Fragment>
+								);
+							default:
+								return null;
+						}
+					case "tool-getWeatherByName":
+						switch (part.state) {
+							case "output-available":
+								return (
+									<Fragment key={`${props.message.id}-${i}`}>
+										<AiToolWeathersList part={part.output} />
+									</Fragment>
+								);
+							default:
+								return null;
+						}
+					case "tool-getWeatherByLocation":
+						switch (part.state) {
+							case "output-available":
+								return (
+									<Fragment key={`${props.message.id}-${i}`}>
+										<AiToolWeathersList part={part.output} />
+									</Fragment>
+								);
+							default:
+								return null;
+						}
 					default:
 						return null;
 				}
